261ef42e3f8c61195a63fe712c87a4c134bb1f9c
unknown <ishika.yadav3@s.amity.edu>
2025-07-13 23:39:40 -0700

    C5

--- scical.c
+++ scical.c
@@ -18,3 +18,6 @@ int main() {
 		case 1: result = num1 + num2; break;
             case 2: result = num1 - num2; break;
             case 3: result = num1 * num2; break;
+		    case 4:
+                if (num2 != 0)
+                    result = num1 / num2;
